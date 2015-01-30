8935d1ecaf9be6e53889dc5f69dce6db2a0ad103
Peter Graham <pwgraham91@gmail.com>
2015-01-29 23:54:39 -0800

    reset migrations

--- restauREST/migrations/0001_initial.py
+++ restauREST/migrations/0001_initial.py
@@ -20,49 +20,17 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(
-                    default=False,
-                    help_text='Designates that this user has all permissions without explicitly assigning them.',
-                    verbose_name='superuser status')),
-                ('username', models.CharField(
-                    help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.',
-                    unique=True,
-                    max_length=30,
-                    verbose_name='username',
-                    validators=[django.core.validators.RegexValidator(
-                        '^[\\w.@+-]+$', 'Enter a valid username.',
-                        'invalid')])),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('username', models.CharField(help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, max_length=30, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')])),
                 ('first_name', models.CharField(max_length=30, verbose_name='first name', blank=True)),
                 ('last_name', models.CharField(max_length=30, verbose_name='last name', blank=True)),
                 ('email', models.EmailField(max_length=75, verbose_name='email address', blank=True)),
-                ('is_staff', models.BooleanField(
-                    default=False,
-                    help_text='Designates whether the user can log into this admin site.',
-                    verbose_name='staff status')),
-                ('is_active', models.BooleanField(
-                    default=True,
-                    help_text='Designates whether this user should be treated as active. '
-                              'Unselect this instead of deleting accounts.',
-                    verbose_name='active')),
-                ('date_joined', models.DateTimeField(
-                    default=django.utils.timezone.now,
-                    verbose_name='date joined')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('restaurant_name', models.CharField(max_length=50)),
-                ('groups', models.ManyToManyField(
-                    related_query_name='user',
-                    related_name='user_set',
-                    to='auth.Group',
-                    blank=True,
-                    help_text='The groups this user belongs to. '
-                              'A user will get all permissions granted to each of his/her group.',
-                    verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(
-                    related_query_name='user',
-                    related_name='user_set',
-                    to='auth.Permission',
-                    blank=True,
-                    help_text='Specific permissions for this user.',
-                    verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
@@ -80,7 +48,7 @@ class Migration(migrations.Migration):
                 ('number_of_females', models.CharField(max_length=2)),
                 ('number_of_children', models.CharField(max_length=2)),
                 ('dinner', models.BooleanField(default=True)),
-                ('weekday', models.BooleanField(default=False)),
+                ('weekday', models.BooleanField(default=True)),
                 ('start_time', models.DateTimeField(auto_now_add=True, null=True)),
                 ('end_time', models.DateTimeField(null=True)),
                 ('total_time', models.CharField(max_length=2, blank=True)),
@@ -96,7 +64,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('table_name', models.CharField(max_length=10)),
                 ('seats', models.SmallIntegerField()),
-                ('restaurant', models.ForeignKey(related_name='restaurants', to=settings.AUTH_USER_MODEL)),
+                ('restaurant', models.ForeignKey(related_name='restaurant_table', to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
@@ -105,7 +73,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='party',
             name='table',
-            field=models.ForeignKey(related_name='tables', to='restauREST.Table'),
+            field=models.ForeignKey(related_name='table_party', to='restauREST.Table'),
             preserve_default=True,
         ),
     ]
