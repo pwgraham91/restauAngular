999b1608b57acd3cc1592586fcebe7ccfebe2c4b
Peter Graham <pwgraham91@gmail.com>
2015-01-30 00:34:34 -0800

    clean up check_datetime

--- restauREST/models.py
+++ restauREST/models.py
@@ -25,13 +25,18 @@ class Party(models.Model):
     number_of_males = models.CharField(max_length=2)
     number_of_females = models.CharField(max_length=2)
     number_of_children = models.CharField(max_length=2)
-    dinner = models.BooleanField(default=(datetime.now().hour >= 16))
-    weekday = models.BooleanField(default=(datetime.now().weekday() >= 6))
+    dinner = models.BooleanField(default=False)
+    weekday = models.BooleanField(default=False)
     start_time = models.DateTimeField(auto_now_add=True, null=True)
-    end_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True, blank=True)
     total_time = models.CharField(max_length=2, blank=True)
-    predicted_end_time = models.DateTimeField(null=True)
+    predicted_end_time = models.DateTimeField(null=True, blank=True)
     table = models.ForeignKey(Table, related_name='table_party')
+
+    def check_datetime(self):
+        self.dinner = datetime.now().hour >= 16
+        self.weekday = datetime.now().weekday() <= 4
+        self.save()
 
     def __unicode__(self):
         return u"pk:{} name:{}".format(self.pk, self.party_name)
